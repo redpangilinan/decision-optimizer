7d64cf9942ae5b6628348bb22b643492a230b83d
redpangilinan <redpangilinan715@gmail.com>
2023-12-29 00:57:31 +0800

    refactor: add necessary info and remove unused code

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,17 +3,16 @@ import { SiteConfig } from "@/types"
 import { env } from "@/env.mjs"
 
 export const siteConfig: SiteConfig = {
-  name: "Next Entree",
+  name: "Quotum",
   author: "redpangilinan",
-  description:
-    "Next.js 14+ starter template with app router, shadcn/ui, typesafe env, icons and configs setup.",
+  description: "Decision optimization web tool",
   keywords: ["Next.js", "React", "Tailwind CSS", "Radix UI", "shadcn/ui"],
   url: {
     base: env.NEXT_PUBLIC_APP_URL,
     author: "https://rdev.pro",
   },
   links: {
-    github: "https://github.com/redpangilinan/next-entree",
+    github: "https://github.com/redpangilinan/quotum",
   },
   ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
 }

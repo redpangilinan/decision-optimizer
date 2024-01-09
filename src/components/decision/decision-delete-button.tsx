83986c1e2da4340f15d1339d52236c85d5292cfa
redpangilinan <redpangilinan715@gmail.com>
2024-01-10 00:16:28 +0800

    feat: add size props on decision delete button

--- src/components/decision/decision-delete-button.tsx
+++ src/components/decision/decision-delete-button.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import { useDecisionStore } from "@/hooks/store/use-decision-store"
-import { ButtonVariants } from "@/types"
 import { toast } from "sonner"
 
+import { ButtonSizes, ButtonVariants } from "@/types/enum"
 import { Button } from "@/components/ui/button"
 import {
   Credenza,
@@ -19,24 +19,21 @@ import { Icons } from "@/components/icons"
 
 interface DecisionDeleteButtonProps {
   variant?: ButtonVariants
+  size?: ButtonSizes
   id: string
 }
 
 export function DecisionDeleteButton({
   variant,
+  size,
   id,
 }: DecisionDeleteButtonProps) {
   const deleteDecision = useDecisionStore((state) => state.deleteDecision)
 
-  const deleteDecisionFn = (id: string) => {
-    deleteDecision(id)
-    toast.success("Decision deleted successfully!")
-  }
-
   return (
     <Credenza>
       <CredenzaTrigger asChild>
-        <Button variant={variant}>
+        <Button variant={variant} size={size}>
           <Icons.delete className="mr-2 h-4 w-4" />
           <span>Delete</span>
         </Button>
@@ -50,7 +47,14 @@ export function DecisionDeleteButton({
         </CredenzaHeader>
         <CredenzaFooter>
           <CredenzaClose asChild>
-            <Button onClick={() => deleteDecisionFn(id)}>Delete</Button>
+            <Button
+              onClick={() => {
+                deleteDecision(id)
+                toast.success("Decision deleted successfully!")
+              }}
+            >
+              Delete
+            </Button>
           </CredenzaClose>
         </CredenzaFooter>
       </CredenzaContent>

5e010d16f319444256dd371aa4b3ebbfc900f338
redpangilinan <redpangilinan715@gmail.com>
2024-01-12 00:27:49 +0800

    feat: add multiplier utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+type Case = {
+  case: string
+  value: number
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
@@ -11,4 +16,13 @@ export function generateId() {
   const id = `${timestamp}-${random}`
 
   return id
+}
+
+export function getMultiplier(choice: string, cases: Case[]): number {
+  for (const item of cases) {
+    if (item.case === choice) {
+      return item.value
+    }
+  }
+  return 0
 }
